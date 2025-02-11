e66db2db2bc9b3d32e3e97ae657783e5d944349c
David Mann <manndm4@gmail.com>
2025-02-11 17:45:21 -0500

    Initial Commit

---
+++ PerfectFitTests/PerfectFitTests.swift
@@ -0,0 +1,17 @@
+//
+//  PerfectFitTests.swift
+//  PerfectFitTests
+//
+//  Created by Mann Fam on 2/11/25.
+//
+
+import Testing
+@testable import PerfectFit
+
+struct PerfectFitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
