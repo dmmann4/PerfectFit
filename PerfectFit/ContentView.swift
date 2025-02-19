0f0c77eb5d7338fb26990ab69cbad6cde169f424
David Mann <manndm4@gmail.com>
2025-02-19 15:57:32 -0500

    inital commit for remote

--- PerfectFit/ContentView.swift
+++ PerfectFit/ContentView.swift
@@ -7,38 +7,146 @@
 
 import SwiftUI
 import SwiftData
+import TabularData
+
+enum ClubType: String, CaseIterable {
+    case wood = "WOODS"
+    case hybrid = "HYBRIDS"
+    case iron = "IRONS"
+    case wedge = "WEDGES"
+    case unknown = ""
+}
+
+struct Shafts {
+    let name: String
+    let clubType: ClubType
+    let code: String
+    let MPF: String
+    let material: String
+    let flex: String
+    let weight: String
+    let torque: String
+    let bendPoint: String
+    let tipStiffness: String
+    let launch: String
+    let price: String
+}
+
 
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var items: [Item]
-
+    @State private var shafts: [Shafts] = []
+    var shaftFilesArray: [String] = ["Woods-Table", "Irons-Table", "Wedges-Table", "Hybrids-Table"]
     var body: some View {
-        NavigationSplitView {
+        NavigationStack {
+            TabView {
+                shaftListView()
+                    .tabItem {
+                        Label("All Shafts", systemImage: "list.dash")
+                    }
+                userSwingDataView()
+                    .tabItem {
+                        Label("My Swing", systemImage: "square.and.pencil")
+                    }
+                   
+            }
+            
+            .navigationTitle("Perfect Fit")
+            .navigationBarTitleDisplayMode(.large)
+            
+        }
+        
+    }
+    
+    private func userSwingDataView() -> some View {
+        Text("User Swing Data")
+    }
+    
+    private func shaftListView() -> some View {
+        VStack(alignment: .leading) {
             List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
+                ForEach(ClubType.allCases, id: \.self) { clubType in
+                    Section(header: Text(clubType.rawValue)) {
+                        ForEach(shafts, id: \.name) { shaft in
+                            if shaft.clubType == clubType {
+                                VStack {
+                                    HStack(alignment: .top) {
+                                        Text(shaft.name)
+                                            .frame(maxWidth: .infinity, alignment: .leading)
+                                            .font(.headline)
+                                            .fontWeight(.heavy)
+                                        Spacer()
+                                        Text(shaft.material)
+                                            .padding(5)
+                                            .background(
+                                                RoundedRectangle(cornerRadius: 10).fill(.gray).opacity(0.3))
+                                            .fontWeight(.bold)
+                                    }
+                                    .padding(.bottom, 24)
+                                    HStack {
+                                        Text(shaft.flex)
+                                        Spacer()
+                                        Text(shaft.launch)
+                                        Spacer()
+                                        Text("\(shaft.weight)g")
+                                    }.fontWeight(.medium)
+                                }
+                            }
+                        }
                     }
                 }
-                .onDelete(perform: deleteItems)
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
+            .onAppear {
+                parseShaftItems()
             }
-        } detail: {
-            Text("Select an item")
         }
     }
-
+    
+    private func parseShaftItems() {
+        var clubType: ClubType = .unknown
+        shaftFilesArray.forEach { file in
+            if let path = Bundle.main.path(forResource: file, ofType: "csv") {
+                let url = URL(fileURLWithPath: path)
+                do {
+                    let data = try Data(contentsOf: url)
+                    let dataEncoded = String(data: data, encoding: .utf8)
+                    if  let dataArr = dataEncoded?.components(separatedBy: "\r\n").map({ $0.components(separatedBy: ";") }) {
+                        for line in 0..<dataArr.count {
+                            let items = dataArr[line][0].components(separatedBy: ",")
+                            
+                            if items.count == 1 {
+                                clubType = ClubType(rawValue: items[0]) ?? .unknown
+                            }
+                            if items.count > 10 {
+                                let shaft = Shafts(
+                                    name: items[0],
+                                    clubType: clubType,
+                                    code: items[1],
+                                    MPF: items[2],
+                                    material: items[3],
+                                    flex: items[4],
+                                    weight: items[5],
+                                    torque: items[6],
+                                    bendPoint: items[7],
+                                    tipStiffness: items[8],
+                                    launch: items[9],
+                                    price: items[10])
+                                shafts.append(shaft)
+                            }
+                        }
+                    }
+                    shafts.forEach { shaft in
+                        print("Type: \(shaft.clubType) - Name: \(shaft.name) - launch: \(shaft.launch)")
+                    }
+                } catch let jsonErr {
+                    print("\n Error reading CSV file: \n ", jsonErr)
+                }
+            }
+        }
+    }
+    
+    
     private func addItem() {
         withAnimation {
             let newItem = Item(timestamp: Date())
@@ -58,4 +166,19 @@ struct ContentView: View {
 #Preview {
     ContentView()
         .modelContainer(for: Item.self, inMemory: true)
+}
+
+
+extension FileManager {
+    func listFiles(path: String) -> [URL] {
+        let baseurl: URL = URL(fileURLWithPath: path)
+        var urls = [URL]()
+        enumerator(atPath: path)?.forEach({ (e) in
+            guard let s = e as? String else { return }
+            let relativeURL = URL(fileURLWithPath: s, relativeTo: baseurl)
+            let url = relativeURL.absoluteURL
+            urls.append(url)
+        })
+        return urls
+    }
 }
