56d957d9126bf15cf4d3b333f67d6e29b36ecb78
David Mann <manndm4@gmail.com>
2025-02-19 20:27:14 -0500

    added sample TCA file to test

---
+++ PerfectFit/SwingDataView.swift
@@ -0,0 +1,18 @@
+//
+//  SwingDataView.swift
+//  PerfectFit
+//
+//  Created by Mann Fam on 2/19/25.
+//
+
+import SwiftUI
+
+struct SwingDataView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwingDataView()
+}
